3e78dd0a01cf645fc00a746d6c346ae73ab62461
Laura <laurazenc@gmail.com>
2019-10-27 13:28:08 +0100

    Electron structure

---
+++ app/store/index.js
@@ -0,0 +1,11 @@
+import configureStoreDev from './store.dev';
+import configureStoreProd from './store.prod';
+
+const selectedConfigureStore =
+  process.env.NODE_ENV === 'production'
+    ? configureStoreProd
+    : configureStoreDev;
+
+export const { configureStore } = selectedConfigureStore;
+
+export const { history } = selectedConfigureStore;
