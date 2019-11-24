d511a60e8b8adcab5f038f9fec2e3246eb1dd314
Laura <laurazenc@gmail.com>
2019-11-24 20:07:03 +0100

    Local path for managing state

---
+++ src/utils/db.ts
@@ -0,0 +1,8 @@
+import fs from 'fs'
+import path from 'path'
+
+try {	
+	if (!fs.existsSync(path.resolve(`./.gitsual`))) fs.mkdirSync(path.resolve(`./.gitsual`))
+} catch (ignore) { } // eslint-disable-line
+
+export const DB_PATH = path.resolve(`./.gitsual/db.json`) // eslint-disable-line
